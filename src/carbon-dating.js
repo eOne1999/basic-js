dc00c546c2c46fd2411cec48249fc015e6f8babe
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2022-10-17 16:22:40 +0300

    feat: implement carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let t;
+  if (typeof sampleActivity !== 'string' ||
+  !((parseFloat(sampleActivity)) && isFinite(sampleActivity)) ||
+  sampleActivity < 0 || sampleActivity > 15) {
+    return false;
+  } else {
+    t = Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity))) * HALF_LIFE_PERIOD / 0.693);
+    return t;
+  }
 }
 
 module.exports = {
