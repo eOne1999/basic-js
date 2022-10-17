a8b1f11feb0a68bf68849b1464e7eb9d3c9c7716
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2022-10-17 16:24:37 +0300

    feat: implement dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let teamName = '';
+  if (members === null || members === undefined) return false;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      let currentItem = members[i].split(' ').join('');
+      teamName += currentItem[0].toUpperCase();
+    }
+  }
+  if (teamName === '') return false;
+
+  function AbcOrder(word) {
+    if (word.length === 0) return '';
+    let left = '',
+      right = '',
+      min = word[0];
+  
+    for (let i = 1; i < word.length; i++) {
+      word[i] < min ?
+      left += word[i] :
+      right += word[i];
+    }
+    return AbcOrder(left).concat(min, AbcOrder(right));
+  }
+
+  return AbcOrder(teamName);
 }
 
 module.exports = {
